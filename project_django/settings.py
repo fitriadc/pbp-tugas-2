fc4f1b116e82a4946cb59a1bb2bd5cd2cc8bad3d
fitriadc <fitriadwicahya@gmail.com>
2022-09-13 14:11:46 +0700

    rehook github action

--- project_django/settings.py
+++ project_django/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
-    directory.mkdir(exist_ok=True)
+    os.makedirs(directory, exist_ok=True)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
