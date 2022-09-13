92a8b78a53edf3c77a8a4ce4f84b6e5199ccac3b
fitriadc <fitriadwicahya@gmail.com>
2022-09-13 13:55:39 +0700

    update program

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+
+def show_catalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Fitria Dwi Cahya',
+    }
+    return render(request, "katalog.html", context)
